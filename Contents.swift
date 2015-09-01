006aa15e45b88cce0026293a1c82e8b66da7d37b
cgrieb <cgrieb@e112-m-11.mchenry.edu>
2015-09-01 17:25:12 -0500

    Constants_And_Variables_CindyG
    Variables and Constants playground assignment

---
+++ Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// create three variables
+var myInt : Int
+var myFloat : Float
+var myDouble : Double
+
+// create three constants
+let thisInt : Int
+let thisFloat : Float
+let thisDouble : Double
+
+// create three explicit constants
+let constString = "Hello World!"
+let constFloat = 5.0
+let constDouble = 0.0000002
+
+// create a constant, initialize then change value
+let testThisConst = "Will it work?"
+// the following code generates an error:
+// "Cannot assign to 'let' value 'testThisConst'
+testThisConst = "nope"
